1bb0d78cee72195e0fb13e5005546137bea8eba3
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-12-28 00:37:23 +0100

    Work on concurrent processes
    and also on looting caches.

--- Resolvable.lua
+++ Resolvable.lua
@@ -2,37 +2,40 @@ Resolvable = Resolvable or {}
 local addOnName, AddOn = ...
 local _ = {}
 
+--- @class Resolvable.Resolvable
 Resolvable.Resolvable = {}
 
-function Resolvable.Resolvable:new(fn)
-  local object = {
+function Resolvable.Resolvable:new()
+  --- @class Resolvable.Resolvable
+  local resolvable = {
     _afterResolve = Hook.Hook:new(),
+    _valuesResolvedWith = nil
   }
-  setmetatable(object, { __index = Resolvable.Resolvable })
-  RunNextFrame(function()
-    local function resolve(...)
-      object._afterResolve:runCallbacks(...)
-    end
-
-    fn(resolve)
-  end)
-  return object
+  resolvable._afterResolve.runCallbacks = Function.once(resolvable._afterResolve.runCallbacks)
+  setmetatable(resolvable, { __index = Resolvable.Resolvable })
+  local resolvableInternal = _.ResolvableInternal:new(resolvable)
+  return resolvable, resolvableInternal
 end
 
 function Resolvable.Resolvable:afterResolve(callback)
-  self._afterResolve:registerCallback(callback)
+  local resolvable, resolvableInternal = Resolvable.Resolvable:new()
+  if self._valuesResolvedWith then
+    local _self = self
+    RunNextFrame(function()
+      resolvableInternal:resolve(callback(unpack(_self._valuesResolvedWith)))
+    end)
+  else
+    self._afterResolve:registerCallback(function(...)
+      resolvableInternal:resolve(callback(...))
+    end)
+  end
+  return resolvable
 end
 
-function Resolvable.Resolvable:resolveWith(...)
-  self._afterResolve:runCallbacks(...)
-end
-
-function Resolvable.Resolvable:resolve()
-  self:resolveWith()
-end
+Resolvable.Resolvable.after = Resolvable.Resolvable.afterResolve
 
 function Resolvable.await(value)
-  if value and value.afterResolve then
+  if type(value) == 'table' and value.afterResolve then
     local thread = coroutine.running()
     value:afterResolve(function(...)
       Coroutine.resumeWithShowingError(thread, ...)
@@ -41,4 +44,39 @@ function Resolvable.await(value)
   else
     return value
   end
+end
+
+function Resolvable.all(resolvables)
+  local resolvable, resolvableInternal = Resolvable.Resolvable:new()
+  local result = {}
+  local numberOfOpenResolvables = Array.length(resolvables)
+  Array.forEach(resolvables, function(resolvable, index)
+    resolvable:after(function(...)
+      result[index] = { ... }
+      numberOfOpenResolvables = numberOfOpenResolvables - 1
+      if numberOfOpenResolvables == 0 then
+        resolvableInternal:resolve(result)
+      end
+    end)
+  end)
+  return resolvable
+end
+
+await = Resolvable.await
+
+_.ResolvableInternal = {}
+
+function _.ResolvableInternal:new(resolvable)
+  local resolvableInternal = {
+    _resolvable = resolvable
+  }
+  setmetatable(resolvableInternal, { __index = _.ResolvableInternal })
+  resolvableInternal.resolve = Function.once(resolvableInternal.resolve)
+  return resolvableInternal
+end
+
+-- TODO: Rename "resolve" to "resolveWith".
+function _.ResolvableInternal:resolve(...)
+  self._resolvable._valuesResolvedWith = { ... }
+  self._resolvable._afterResolve:runCallbacks(...)
 end
